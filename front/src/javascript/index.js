ff2cc13e4be410689dbcbe22fbfae5f8b3ed3d28
curso04 <curso04@gmail.com>
2024-12-13 12:30:24 +0100

    Replanteando bootstrap. Problemas versión anterior.

--- front/src/javascript/index.js
+++ front/src/javascript/index.js
@@ -1,9 +1,65 @@
 $(document).ready(function() {
+
 // https://my-json-server.typicode.com/juanmgp888/myjsonserver/solicitudes
 
+    // listar registros
+    function listar() {
+        $.get("https://my-json-server.typicode.com/juanmgp888/myjsonserver/solicitudes", function(data) {
+            $("#registros").empty();  // Limpiar el contenido antes de agregar nuevos registros
+            console.log(data);
+    
+            data.forEach(function(solicitud) {
+                $("#registros").append(
+                    $("<li>")
+                        .text(solicitud.nombre + ' ' + solicitud.intento + ' ' + 
+                            solicitud.contador + ' ' + solicitud.adivinar)
+                        .val(solicitud)  // Establecer el valor del elemento (esto puede ser opcional)
+                        .attr("id", "id" + solicitud.id)  // Asignar un ID único al elemento
+                );
+            });
+        
+        }).fail(function() {
+            alert("Error al cargar las solicitudes.");
+        });
+    } // fin bloque listar registros
 
 
+    // Crear
+    $("#nuevaPartida").on("click",function(){
+        $("#divNombre").show();
+        $("#divIntento").show();
+    
+        var nombreNuevo = $("#nombre").val();
+        var intentoNuevo = $("#intento").val();
+        var contaIntentosNuevo = 1;
+   //     var adivinarNuevo = 77;   // generar aleatoriamente en cada nueva partida
+        if (nombreNuevo === "") { // Ni vacíos ni de tipo distinto a cadena
+            alert("Por favor, introduzca un nombre");
+            return;
+        }
+        $.ajax({
+            url: "https://my-json-server.typicode.com/juanmgp888/myjsonserver/solicitudes",
+            method: "POST",
+            "data": JSON.stringify({
+                id: 0,          // Cero indica creación 
+                nombreJugador: nombreNuevo,
+                numIntento: intentoNuevo,  // pte. añadir a un array de intentos
+                contaIntentos: contaIntentosNuevo,
+            //    adivinar: adivinaNum;
+            }),
+            success: function(data) {
+                $("#resCrear").text("OK. Nuevo registro."); 
+                listar();
+                console.log(data);
+            },
+            error: function(data) {
+                console.log(data);
+            }
+        }); 
 
+    }) // fin bloque nueva partida
+
+    
 });
 
-console.log("nada");
+console.log("jquery funcionando");
