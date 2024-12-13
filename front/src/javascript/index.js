e1967d35d8a6a293e3a41b0a834ffe4ff66b4852
Juanma <Juanma888@gmail.com>
2024-12-13 16:45:57 +0100

    Botones 'crear' y 'cancelar partida' operativos

--- front/src/javascript/index.js
+++ front/src/javascript/index.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("#nombreJugador").val("");
     $("#divNombre").show();  // Muestra input para que el jugador introduzca el nombre
     $("#nombreJugador").focus(); 
-    $("#nuevaPartida").show();
+    $("#btnNuevaPartida").show();
 
     // listar registros
     function listar() {
@@ -28,34 +28,34 @@ $(document).ready(function() {
         });
     } // fin bloque listar registros
 
-    // Crear nueva partida
-    $("#nuevaPartida").on("click", function() {
+    // Crear nueva partida ************************************************************************
+    $("#btnNuevaPartida").on("click", function() {
 
     var nombreNuevo = $("#nombreJugador").val();
     //     var adivinarNuevo = 77;   // generar aleatoriamente en cada nueva partida
          if (nombreNuevo === "") { // Ni vacíos ni de tipo distinto a cadena
-            $("#resCrear").text("Introduce un nombre si quieres retarme.");
+            $("#respuestaServidor").text("Introduce un nombre si quieres retarme.");
             $("#nombreJugador").focus(); 
             return;
          } else {
-            $("#nuevaPartida").prop("disabled", true);  // Deshabilitar el botón
-             var intentoNuevo = $("#intento").val();
-             var contaIntentosNuevo = 1;
-             $("#divIntento").show();
-             
+            $("#btnNuevaPartida").prop("disabled", true);  // Deshabilitar el botón
+            var intentoNuevo = $("#intento").val();
+            var contaIntentosNuevo = 1;
+            $("#divIntento").show();
+            $("#btnsIntentarYCancelar").show();
          }
         $.ajax({
             url: "https://my-json-server.typicode.com/juanmgp888/myjsonserver/solicitudes",
             method: "POST",
             "data": JSON.stringify({
-                id: 0,          // Cero indica creación 
+                id: 0,     // Cero indica creación 
                 nombreJugador: nombreNuevo,
                 numIntento: intentoNuevo,  // pte. añadir a un array de intentos
                 contaIntentos: contaIntentosNuevo,
             //    adivinar: adivinaNum;
             }),
             success: function(data) {
-                $("#resCrear").text("OK. Nuevo registro."); 
+                $("#respuestaServidor").text("OK. Nuevo registro."); 
              //   listar();
                 console.log(data);
             },
@@ -64,6 +64,23 @@ $(document).ready(function() {
             }
         }); 
     }) // fin bloque nueva partida
+
+    $("#cancelar").on("click", function(){
+        // Volver a la situación inicial de la pantalla. Ocultar elementos.
+    
+        
+        $("#divIntento").hide();    // ocultar bloque que pregunta el número que quieres probar
+        $("#btnsIntentarYCancelar").hide(100); // ocultar botones intentar (adivinar) y cancelar partida
+
+        $("#divNombre").show();  // Muestra input para que el jugador introduzca el nombre
+        $("#nombreJugador").focus(); 
+      //  $("#btnNuevaPartida").show(); // Mostrar botón nueva partida
+        $("#btnNuevaPartida").prop("disabled", false);  // habilitar el botón nueva partida
+        $("#nombreJugador").val(""); // Vaciar input de entrada de nombre del jugador
+        $("#respuestaServidor").val("").text("");
+
+
+    })
 });
 
 console.log("jquery funcionando");
