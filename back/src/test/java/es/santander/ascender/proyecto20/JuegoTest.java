fb83c6d454933e0d0ddbfb475ea9b8e10d63bc9b
rodrigo <rocasgut@gmail.com>
2024-12-14 17:35:02 +0100

    test nuevos clase juego

--- back/src/test/java/es/santander/ascender/proyecto20/JuegoTest.java
+++ back/src/test/java/es/santander/ascender/proyecto20/JuegoTest.java
@@ -53,6 +53,26 @@ public class JuegoTest {
 
     }
 
+    @Test //test para comprobar que el numero objetivo esta en el rango de 0 a 100
+    void testNumeroObjetivoRango(){
+
+        int Numobjetivo = jueguito.getNumTarget();
+        assertTrue(Numobjetivo >= 1 && Numobjetivo <=100, "el numero objetivo debe estar entre 0 y 100");
+
+    }
+    @Test //test para comprobar que si el jugador sobrepasa el numero de intentos le lanze una excepcion
+    void testIntentosMaximos(){
+        try{
+            for (int i = 0; i < 10; i++) {
+
+                jueguito.jugarIntento(50, 1);
+
+            }
+        } catch(MiExcepcion e) {
+            assertEquals("No quedan mas intentos", e.getMessage());
+        }
+    }
+
     // @Test
     // void testLeerRegistro() {
 
