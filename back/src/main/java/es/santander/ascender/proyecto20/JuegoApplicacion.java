1c1d509b80f18583f7aec5e9cdf9bb11e97bf365
curso07 <alejandro.cald@gmail.com>
2024-12-14 18:31:49 +0100

    primer avnace on rest controller

---
+++ back/src/main/java/es/santander/ascender/proyecto20/JuegoApplicacion.java
@@ -0,0 +1,12 @@
+package es.santander.ascender.proyecto20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoApplicacion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuegoApplicacion.class, args);
+    }
+}
---
+++ back/src/main/java/es/santander/ascender/proyecto20/controller/JuegoController.java
@@ -0,0 +1,42 @@
+package es.santander.ascender.proyecto20.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import es.santander.ascender.proyecto20.Juego;
+import es.santander.ascender.proyecto20.MiExcepcion;
+
+@RestController
+@RequestMapping("/juego")
+public class JuegoController {
+
+    private Juego juego;
+
+    /**
+     * @param usuario
+     * @return
+     */
+    @GetMapping("/start")
+    public int inicializar(String usuario) {
+        int sesion = juego.inicializarJuego();  
+        return sesion;
+    }
+
+    /**
+     * @param number
+     * @return
+     * @throws MiExcepcion
+     */
+    @GetMapping("/guess/{number}")
+    public int[] guessNumber(@PathVariable int number) throws MiExcepcion {
+        int[] result = juego.jugarIntento(number, number);
+            return result;
+    }
+
+    /**
+     * @return
+     */
+    @GetMapping("/attempts")
+    public int intentos() {
+        return juego.getNumTarget();
+    }
+}
