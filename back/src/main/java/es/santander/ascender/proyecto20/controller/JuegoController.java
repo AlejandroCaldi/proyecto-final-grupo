9f3b658e6bd0c7eb8e185f2c93320d1ef416e826
curso07 <alejandro.cald@gmail.com>
2024-12-15 19:08:37 +0100

    Pude linkear un caso, el de inicio, pero hay problemas de CORS en los otros dos, lo tenemos que ver en clase.

--- back/src/main/java/es/santander/ascender/proyecto20/controller/JuegoController.java
+++ back/src/main/java/es/santander/ascender/proyecto20/controller/JuegoController.java
@@ -19,27 +19,29 @@ public class JuegoController {
     private long sesion = 0;
 
     /**
-     *Empieza el juego inicializando el objeto juego, preservando el httoSession como id de sesión. .
-     * @param getUsuario El nombre de jugador que ha pasado el cliente. 
-     * @param session La sesión de Http que servirá como identificador. 
-     * @return id de Sesión. 
-          * @throws Exception 
-          */
-         @PostMapping("/inicio")
-         public ResponseEntity<Map<String, Object>> iniciarJuego(@RequestBody String getUsuario, HttpSession session) throws Exception {
-           
-            Juego juego = new Juego(getUsuario);
-            
-            this.sesion = juego.inicializarJuego();
-            
-            session.setAttribute("juego", juego);
-            session.setAttribute("sessionID", this.sesion);
+    *Empieza el juego inicializando el objeto juego, preservando el httoSession como id de sesión. .
+    * @param getUsuario El nombre de jugador que ha pasado el cliente. 
+    * @param session La sesión de Http que servirá como identificador. 
+    * @return id de Sesión. 
+    * @throws Exception 
+    */
+    @CrossOrigin(origins = "http://localhost:1234")
+    @RequestMapping(value = "/inicio", method = RequestMethod.POST)
+    @PostMapping("/inicio")
+    public ResponseEntity<Map<String, Object>> iniciarJuego(@RequestBody String getUsuario, HttpSession session) throws Exception {
+    
+    Juego juego = new Juego(getUsuario);
+    
+    this.sesion = juego.inicializarJuego();
+    
+    session.setAttribute("juego", juego);
+    session.setAttribute("sessionID", this.sesion);
 
-            Map<String, Object> response = new HashMap<>();
-            response.put("sessionID", this.sesion);
+    Map<String, Object> response = new HashMap<>();
+    response.put("sessionID", this.sesion);
 
-            return ResponseEntity.ok(response);
-    }
+    return ResponseEntity.ok(response);
+}
 
     /**
      * Cancela el juego y envía el número que se debia haber adivinado. 
@@ -47,6 +49,8 @@ public class JuegoController {
      * @param session The HTTP session to retrieve the game instance.
      * @return A response containing the result of cancelling the game.
      */
+    @CrossOrigin(origins = "http://localhost:1234")
+    @RequestMapping(value = "/cancelar", method = RequestMethod.PUT)
     @PutMapping("/cancelar")
     public ResponseEntity<Map<String, Number>> cancelarJuego(@RequestBody Map<String, Long> payload, HttpSession session) {
 
@@ -70,9 +74,11 @@ public class JuegoController {
      *         se trta de -1 si el dato es menor, 0 si acertó, o 1 s el número es mayor. 
      * @throws MiExcepcion. Para el caso de que se pase un texto o un valor menor a 0 o mayor a 100. 
      */
-    @PostMapping("/adivinar/")
-    public ResponseEntity<Map<String, Number>> adivinaNumero(@RequestBody int numero, HttpSession session) throws MiExcepcion {
+    @CrossOrigin(origins = "http://localhost:1234")
+    @PostMapping("/adivinar")
+    public ResponseEntity<Map<String, Number>> adivinaNumero(@RequestBody Map<String, Integer> payload, HttpSession session) throws MiExcepcion {
 
+        int numero = payload.get("numero");
         long sessionID = (long) session.getAttribute("sessionID");
         Juego juego = (Juego) session.getAttribute("juego");
 
