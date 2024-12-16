1d72a818882dfd07591963f395ad9445d082a31d
curso07 <alejandro.cald@gmail.com>
2024-12-16 13:01:49 +0100

    Versión recontra definitiva

--- back/src/main/java/es/santander/ascender/proyecto20/controller/JuegoController.java
+++ back/src/main/java/es/santander/ascender/proyecto20/controller/JuegoController.java
@@ -3,8 +3,6 @@ package es.santander.ascender.proyecto20.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +24,6 @@ public class JuegoController {
     /**
     *Empieza el juego inicializando el objeto juego, preservando el httoSession como id de sesión. .
     * @param getUsuario El nombre de jugador que ha pasado el cliente. 
-    * @param session La sesión de Http que servirá como identificador. 
     * @return id de Sesión. 
     * @throws Exception 
     */
@@ -49,7 +46,8 @@ public class JuegoController {
     /**
      * Cancela el juego y envía el número que se debia haber adivinado. 
      * 
-     * @param session @return A response containing the result of cancelling the game.
+     * @param partida el numero de sesion
+     * @return el valor qeu habìa que adivinar. 
      */
     @PostMapping("/cancelar")
     public ResponseEntity<Map<String, Number>> cancelarJuego(@RequestBody Partida partida) {
@@ -67,13 +65,11 @@ public class JuegoController {
     }
 
     
+
     /**
-     * @param numero el numero arriesgado por el jugador, es un entero entre 0 y 100. 
-     * @param request datos se sesión de HTTP
-     * @param session el identificador único de sesión HTTP, usado como Id de instancia de juego cuando es web. 
-     * @return ResponseEntity<Map<String, Number>> con los valores enteros de respuesta e intento. En el caso de respuesta
-     *         se trta de -1 si el dato es menor, 0 si acertó, o 1 s el número es mayor. 
-     * @throws MiExcepcion. Para el caso de que se pase un texto o un valor menor a 0 o mayor a 100. 
+     * @param guess el valor arriesgado por el usuario
+     * @return retora si acertó y la cantiadd de intentos que lleva la sesiòn
+     * @throws MiExcepcion en cas de que el numero estè fuera de rango. 
      */
     @PostMapping("/adivinar")
     public ResponseEntity<Map<String, Number>> adivinaNumero(@RequestBody Guess guess) throws MiExcepcion {
